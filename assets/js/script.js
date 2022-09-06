d8fb7ed22725d4544a2486de378b0c183c7b778f
HuseynJafarov <huseynnj@code.edu.az>
2022-09-06 20:08:28 +0400

    Jquery-Practic

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// $(document).ready(function(){
+
+//   });
+
+
+// $(function () {
+//   //let text = document.querySelector("#product .text h1").innerText
+//   //let elem = $("h1");
+// //   let text = $("#product .text h1");
+// //   console.log(text);
+
+// //   for (const item of text) {
+// //     console.log(item);
+// //   }
+//     // $("#product .text button").first().click(function(){
+//     //     console.log("salam 999");
+//     // })
+//     $("#product .text button").first().click(function(){
+//         // console.log($(this).html(`<span class="text-danger" >A.y.E</span>`));
+//         $("#product .text .box").hide(2000);
+//         $("#product .text h1").css({    
+//         "color":"blue",
+//         "font-size":"80px"
+//     })
+//     })
+//     $("#product .text button").first().next().click(function(){
+//         $("#product .text .box").show(2000,function(){
+//             $("#product .text .box").css("background-color","blue")
+//         });
+//     })
+//     $("#product .text button").last().click(function(){
+//         $("#product .text .box").toggle(1000);
+//     })
+
+
+
+
+
+});
